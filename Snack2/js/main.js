7538eaa618155bbbc697dec8ba0b294243e23f59
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-01-30 15:14:52 +0100

    Snack 2

---
+++ Snack2/js/main.js
@@ -0,0 +1,18 @@
+// TRACCIA
+// Crea un array vuoto e chiedi all'utente un numero da inserire nell'array.Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
+
+// SVOLGIMENTO
+// Crea array vuoto
+const numbers = [];
+
+let somma = 0;
+// FINCHE' la somma degli elementi è minore di 50
+while (somma < 50) {
+    // Chiedi all'utente numero da inserire nell'array
+    let userNumber = parseInt(prompt("Scegli un numero"));
+    somma += userNumber;
+    // Inserisci numero nell'array iniziale
+    numbers.push(userNumber);
+}
+
+console.log(numbers);
