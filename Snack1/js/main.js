b1b44fc398da6b94a79e823620fdbac23662b1f1
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-01-31 09:51:08 +0100

    fixes Snack 1

--- Snack1/js/main.js
+++ Snack1/js/main.js
@@ -2,19 +2,14 @@
 // Chiedi all'utente un valore minimo ed un valore massimo. Genera un numero casuale all'interno del range fornito dall'utente.
 
 // SVOLGIMENTO
+let outputMessageEl = document.getElementById("output_message");
+let messageText;
 // Chiedi all'utente un valore minimo
 let userMinNumber = parseInt(prompt("Scegli un numero come range minimo"));
 // Chiedi all'utente un valore massimo
 let userMaxNumber = parseInt(prompt("Scegli un numero come range massimo"));
-
-let outputMessageEl = document.getElementById("output_message");
-let messageText;
-
 // Genera numero all'interno del range
-let randomNumbers = Math.floor(Math.random() * 100) + 1;
-if (randomNumbers <= userMaxNumber && randomNumbers >= userMinNumber) {
-    messageText = randomNumbers;
-} else {
-    messageText = "Il numero generato non rientra nel range";
-}
+let randomNumbers = Math.floor(Math.random() * (userMaxNumber - userMinNumber) + 1) + userMinNumber;
+messageText = randomNumbers;
+
 outputMessageEl.innerHTML = messageText;
